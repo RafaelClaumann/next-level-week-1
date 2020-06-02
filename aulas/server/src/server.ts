2d869b051f05946529cd913c0905cb4ec798ff39
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2020-06-02 09:26:02 -0300

    criando rota para retornar apenas um registro

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -2,8 +2,37 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ mensagem: 'Hello World' })
+const users = [
+  {
+    nome: 'rafael',
+    email: 'rafael@email.com.br'
+  },
+  {
+    nome: 'douglas',
+    email: 'douglas@email.com.br'
+  },
+  {
+    nome: 'guilherme',
+    email: 'guilherme@email.com.br'
+  }
+]
+
+// GET: http://localhost:3333/users => retornar uma lista de usuarios.
+// GET: http://localhost:3333/users/1 => retornar um de usuario com id = 1.
+
+// retorna a lista de usuarios completa
+app.get('/users', (request, response) => {
+  return response.json(users)
+})
+
+// :id em '/users/:id' é o parametro da rota chamado.
+// request.params.id usado para acessar os parametros da rota, id neste caso.
+// id é recebido como uma string '1' ou '2' etc.
+// id é usado como indice do array por isso é convertido em numero.
+// localhost:3333/0 retorna {name: 'rafael', email: 'rafael@email.com.br'}
+app.get('/users/:id', (request, response) => {
+  var id = Number(request.params.id)
+  return response.json(users[id])
 })
 
 app.listen(3333, () => { console.log('🚀 !backend iniciado! 🚀') })
