5bb5a695712f669fc69c9e767f2ceaf3923b7235
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2020-06-07 19:54:39 -0300

    exemplo estado e imutabilidade react com typescript

--- aulas/web/src/HeaderExample.tsx
+++ aulas/web/src/HeaderExample.tsx
@@ -1,6 +1,6 @@
 // Este arquivo serve apenas para estudo dos componentes com Typescript.
 // Não deve ser levado em consideração e provavelmente será deletado.
-import React from 'react'
+import React, { useState } from 'react'
 
 interface HeaderExampleProps {
   nome: string  // parametro obrigatorio
@@ -9,8 +9,23 @@ interface HeaderExampleProps {
 
 const HeaderExample: React.FC<HeaderExampleProps> = (props) => {
   //props = {nome: 'nome', sobrenome: 'sobrenome'}
+
+  // ESTADO: Informações mantidas pelo próprio componente.
+  // useState retorna um array com o valor inicial do estado e funcao para atualizar
+  const [counter, setCounter] = useState<number>(0);
+
+  function handleCounter() {
+    // IMUTABILIDADE: Não é possivel alterar o valor de um estado diretamente.
+    // counter++, counter = counter + 1, isso não funciona.
+    setCounter(counter + 1)
+  }
+
   return (
-    <h1>{props.nome} {props.sobrenome}</h1>
+    <>
+      <h1>{props.nome} {props.sobrenome}</h1>
+      <h2>counter: {counter}</h2>
+      <button type="button" onClick={handleCounter}>Incrementar</button>
+    </>
   );
 }
 
