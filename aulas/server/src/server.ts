d243b959b5ce8546b12db69c4729034a250ea9e7
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2020-06-08 19:44:47 -0300

    buscando itens de coleta da API node

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import routes from './routes'
 import path from 'path'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(express.json())  // express passa a entender RequestBody em formato JSON
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
